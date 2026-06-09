e8e8d8a2dacfaf990f43025ca7670d7d76e5fcec
dawit3091 <dawita471@gmail.com>
2026-06-09 11:33:40 -0600

    finished the logic of the calculator adding,sub,multi,dividing

--- script.js
+++ script.js
@@ -31,3 +31,50 @@ function operate(operator,a,b) {
         return divide(num1,num2);
     }
 }
+
+
+const displayText=document.getElementById('text_display');
+const numberBtn=document.querySelectorAll('[data-number]');
+const equalBtn=document.querySelector('.equal');
+const operatorBtn=document.querySelectorAll('[data-operator]');
+
+
+numberBtn.forEach(button =>{
+    button.addEventListener('click', (e) =>{
+        const clickedNum=e.target.dataset.number;
+
+        if (operator===null) {
+            first_num+=clickedNum;
+            displayText.textContent=first_num;
+            
+        }else{
+            second_num+=clickedNum;
+            displayText.textContent=second_num;
+        }
+    });
+});
+
+
+equalBtn.addEventListener('click', ()=>{
+    if (first_num !=='' && second_num!=='' && operator!==null) {
+        let result=operate(operator,first_num,second_num);
+        displayText.textContent=result;
+
+        first_num=result.toString();
+        second_num='';
+        operator=null;
+    }
+
+});
+
+
+operatorBtn.forEach(button =>{
+    button.addEventListener('click',(e) =>{
+        if (first_num !== '') {
+            operator=e.target.dataset.operator;
+            displayText.textContent=operator;
+            
+        }
+
+    });
+});
