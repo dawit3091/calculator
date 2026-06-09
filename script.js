f2b4d729b7619790ccb5c56adb947a246e4d64d3
dawit3091 <dawita471@gmail.com>
2026-06-09 06:53:13 -0600

    finished oprator function

---
+++ script.js
@@ -0,0 +1,31 @@
+let first_num='';
+let second_num='';
+let operator=null;
+//operation functions
+function add(a,b) {
+    return a + b;
+}
+function subtract(a,b) {
+    return a - b;
+}
+function divide(a,b) {
+    return a / b;
+}
+function multiply(a,b) {
+    return a * b;
+}
+//orator function that connects and calls the obove functions
+function operator(operator,a,b) {
+    let num1=Number(a);
+    let num2=Number(b);
+    if (operator==='+') {
+       return add(num1,num2);
+    } else if (operator==='-') {
+        return subtract(num1,num2);
+    } else if(operator==='*') {
+        return multiply(num1,num2);
+    }
+    else if (operator==='/') {
+        return divide(num1,num2);
+    }
+}
