9ce3518d0affce9dc14ef961013783006b313617
dawit3091 <dawita471@gmail.com>
2026-06-11 06:55:03 -0600

    complete calculator JS logic and css stayle with desimal point

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ let first_num='';
 let second_num='';
 let operator=null;
 //operation functions
+
+
+let powerOn=true;
+
+
+
+
+
+
 function add(a,b) {
     return a + b;
 }
@@ -37,11 +46,37 @@ const displayText=document.getElementById('text_display');
 const numberBtn=document.querySelectorAll('[data-number]');
 const equalBtn=document.querySelector('.equal');
 const operatorBtn=document.querySelectorAll('[data-operator]');
+const clearBtn=document.querySelector('.clear');
+const powerBtn=document.querySelector('.power');
+
+powerBtn.addEventListener('click', () =>{
+    if (powerOn) {
+        powerOn=false;
+        powerBtn.textContent='ON';
+        displayText.textContent='';
+
+        first_num='';
+        second_num='';
+        operator=null;
+        
+    } else {
+        powerOn=true;
+        powerBtn.textContent='OFF';
+
+        displayText.textContent=0;
+
+    }
+});
+
 
 
 numberBtn.forEach(button =>{
     button.addEventListener('click', (e) =>{
         const clickedNum=e.target.dataset.number;
+
+        if(!powerOn){
+            return
+        }
 
         if (operator===null) {
             first_num+=clickedNum;
@@ -56,6 +91,13 @@ numberBtn.forEach(button =>{
 
 
 equalBtn.addEventListener('click', ()=>{
+
+    if(!powerOn){
+            return
+        }
+
+
+
     if (first_num !=='' && second_num!=='' && operator!==null) {
         let result=operate(operator,first_num,second_num);
         displayText.textContent=result;
@@ -69,6 +111,15 @@ equalBtn.addEventListener('click', ()=>{
 
 
 operatorBtn.forEach(button =>{
+
+
+
+    if(!powerOn){
+            return
+        }
+
+
+        
     button.addEventListener('click',(e) =>{
         if (first_num !== '') {
             operator=e.target.dataset.operator;
@@ -77,4 +128,13 @@ operatorBtn.forEach(button =>{
         }
 
     });
+});
+
+
+clearBtn.addEventListener('click', () =>{
+    first_num='';
+    second_num='';
+    operator=null;
+    displayText.textContent=0;
+
 });
